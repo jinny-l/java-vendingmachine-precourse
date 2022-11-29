f2bd7ad476a27d527bebe58202a71c0641c88458
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 14:46:46 +0900

    feat: Coin 객체 정보의 getter 메서드 구현

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,49 @@
 package vendingmachine;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500, 0),
+    COIN_100(100, 0),
+    COIN_50(50, 0),
+    COIN_10(10, 0);
 
     private final int amount;
+    private int count;
 
-    Coin(final int amount) {
+    Coin(final int amount, int count) {
         this.amount = amount;
+        this.count = count;
     }
 
-    // 추가 기능 구현
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static List<Integer> getCoinsAmount() {
+        return Stream.of(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> getCoinsCount() {
+        return Stream.of(Coin.values())
+                .map(Coin::getCount)
+                .collect(Collectors.toList());
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+    public void decreaseCount() {
+        count--;
+    }
+
 }
