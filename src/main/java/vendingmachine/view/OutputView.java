07dcbfeee6077c805168d0e917f1b9d33474a6e6
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 17:43:50 +0900

    fix: 투입 금액 출력 시 단위가 출력되지 않는 이슈 수정

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,9 +1,11 @@
 package vendingmachine.view;
 
+import vendingmachine.Coin;
+
 public class OutputView {
 
     private static final String OUTPUT_VENDING_MACHINE_COINS = "자판기가 보유한 동전";
-    private static final String OUTPUT_CHANGES = "투입 금액: %s%n";
+    private static final String OUTPUT_CHANGES = "투입 금액: %s%s%n";
     private static final String OUTPUT_RESULT = "잔돈%n%s%n";
 
     public static void printVendingMachineCoins(String coinsInfo) {
@@ -12,7 +14,7 @@ public class OutputView {
     }
 
     public static void printChanges(int changes) {
-        System.out.printf(OUTPUT_CHANGES, changes);
+        System.out.printf(OUTPUT_CHANGES, changes, Coin.getCoinUnit());
     }
 
     public static void printResult(String coinsResult) {
