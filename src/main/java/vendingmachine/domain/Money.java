ed683f3c44cfdc6ce33d685151f84f3f5e5b1a3f
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 17:41:15 +0900

    feat: Money 입력 값의 유효성을 검증하는 기능 구현

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,12 +1,17 @@
 package vendingmachine.domain;
 
 import java.util.List;
+import vendingmachine.information.ErrorMessage;
 
 public class Money {
 
-    int money;
+    private static int MIN_MONEY = 10;
+
+    private int money;
 
     public Money(int money) {
+        validateMinMoney(money);
+        validateMoneyUnit(money);
         this.money = money;
     }
 
@@ -16,6 +21,18 @@ public class Money {
 
     public void decreaseMoney(List<Integer> money) {
         this.money -= money.get(0);
+    }
+
+    private void validateMinMoney(int money) {
+        if (money < MIN_MONEY) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_ERROR.getMessage());
+        }
+    }
+
+    private void validateMoneyUnit(int money) {
+        if (money % MIN_MONEY != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_ERROR.getMessage());
+        }
     }
 
 }
