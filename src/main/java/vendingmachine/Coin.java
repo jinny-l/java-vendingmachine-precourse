8be7eabe0babf37438a67a6b0ceb5f76dd8d442b
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 17:56:02 +0900

    feat: 자판기가 보유한 동전 정보를 리턴하는 기능 구현

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -10,12 +10,20 @@ public enum Coin {
     COIN_50(50, 0),
     COIN_10(10, 0);
 
+    private static final String COIN_UNIT = "원";
+    private static final String INFO_DELIMITER = " - ";
+    private static final String COUNT_UNIT = "개";
+
     private final int amount;
     private int count;
 
     Coin(final int amount, int count) {
         this.amount = amount;
         this.count = count;
+    }
+
+    public static String getCoinUnit() {
+        return COIN_UNIT;
     }
 
     public int getAmount() {
@@ -37,6 +45,37 @@ public enum Coin {
                 .map(Coin::getCount)
                 .collect(Collectors.toList());
     }
+
+    public static String getCoinsInfo() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < Coin.values().length; i++) {
+            stringBuilder.append(Coin.values()[i].getAmount())
+                    .append(COIN_UNIT)
+                    .append(INFO_DELIMITER)
+                    .append(Coin.values()[i].getCount())
+                    .append(COUNT_UNIT)
+                    .append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    // TODO: 잔돈을 출력하는 기능 구현
+//    private String payCoin(int changes, int ordinal) {
+//        int coinSum = getCoinSum();
+//        int count;
+//        this.count = changes / Coin.values()[i].getAmount()
+//        }
+//
+//    }
+
+    private int getCoinSum() {
+        int sum = 0;
+        for (int i = 0; i < Coin.values().length; i++) {
+            sum += Coin.values()[i].getAmount() * Coin.values()[i].getCount();
+        }
+        return sum;
+    }
+
 
     public void increaseCount() {
         count++;
