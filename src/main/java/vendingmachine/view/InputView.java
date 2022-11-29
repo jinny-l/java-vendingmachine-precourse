64f59541c0accb81a2e70e7c3e23c174748fb9b5
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 16:14:50 +0900

    feat: 구매할 상품명을 입력하는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String INPUT_COINS = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String INPUT_MONEY = "투입 금액을 입력해 주세요.";
+    private static final String INPUT_PRODUCT = "구매할 상품명을 입력해 주세요.";
 
     public static int readCoins() {
         System.out.println(INPUT_COINS);
@@ -32,6 +33,11 @@ public class InputView {
         } catch (NumberFormatException ne) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static String readProduct() {
+        System.out.println(INPUT_PRODUCT);
+        return input();
     }
 
     private static String input() {
