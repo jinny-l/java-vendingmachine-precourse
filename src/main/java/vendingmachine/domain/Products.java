67dbcc2883ddc0d285b6d6c3430d041ec4d1b1ad
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 17:34:24 +0900

    feat: 모든 상품의 가격을 반환하는 getter 기능 구현

--- src/main/java/vendingmachine/domain/Products.java
+++ src/main/java/vendingmachine/domain/Products.java
@@ -1,15 +1,22 @@
 package vendingmachine.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Products {
 
-    private List<Product> products;
+    private final List<Product> products = new ArrayList<>();
 
     public List<Integer> getProductPrice(String input) {
         return products.stream()
                 .filter(product -> product.getName().equals(input))
+                .map(Product::getPrice)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getProductsPrice() {
+        return products.stream()
                 .map(Product::getPrice)
                 .collect(Collectors.toList());
     }
