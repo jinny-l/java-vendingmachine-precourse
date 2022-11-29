c2a75254b52b67ceb93addfb33603829acea0690
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 16:41:48 +0900

    feat: 잔돈을 출력하는 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -4,14 +4,19 @@ public class OutputView {
 
     private static final String OUTPUT_VENDING_MACHINE_COINS = "자판기가 보유한 동전";
     private static final String OUTPUT_CHANGES = "투입 금액: %s%n";
+    private static final String OUTPUT_RESULT = "잔돈%n%s%n";
 
     public static void printVendingMachineCoins(String coinsInfo) {
         System.out.println(OUTPUT_VENDING_MACHINE_COINS);
         System.out.println(coinsInfo);
     }
 
-    private static void printChanges(String changes) {
+    public static void printChanges(int changes) {
         System.out.printf(OUTPUT_CHANGES, changes);
+    }
+
+    public static void printResult(String coinsResult) {
+        System.out.printf(OUTPUT_RESULT, coinsResult);
     }
 
 }
