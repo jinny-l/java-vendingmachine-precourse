a1209730a8f127decc23ff59f9d0c64621e2ddf9
jinny-l <jinny.l.dev@gmail.com>
2022-11-29 13:55:22 +0900

    feat: 자판기 보유 금액, 상품, 투입 금액 입력 기능 구현

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,49 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String INPUT_COINS = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INPUT_MONEY = "투입 금액을 입력해 주세요.";
+
+    public static int readCoins() {
+        System.out.println(INPUT_COINS);
+        String input = input();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static String readProducts() {
+        System.out.println(INPUT_PRODUCTS);
+        String input = input();
+        return input;
+    }
+
+    public static int readMoney() {
+        System.out.println(INPUT_MONEY);
+        String input = input();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static String input() {
+        String input = Console.readLine().trim();
+        validateHasInput(input);
+        return input;
+    }
+
+    private static void validateHasInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
