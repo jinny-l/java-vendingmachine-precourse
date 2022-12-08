c85b39ce61915a3d009cb9e94d3cc609587aa420
jinny-l <jinny.l.dev@gmail.com>
2022-12-08 21:07:50 +0900

    refactor: count 관련 기능 제거
    * 이유:
    count는 변경되는 숫자로, enum에서 관리하기 부적절하다고 판단

---
+++ src/main/java/vendingmachine/constant/Coin.java
@@ -0,0 +1,36 @@
+package vendingmachine.constant;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private static final String COIN_UNIT = "원";
+
+    private final int amount;
+
+    Coin(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static List<Integer> getCoinsAmount() {
+        return Stream.of(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return amount + COIN_UNIT;
+    }
+
+}
